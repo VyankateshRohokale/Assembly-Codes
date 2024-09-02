6eeacf3eef337a7c5f1f8803e961af611e544bcf
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-09-02 10:20:22 -0700

    Add files via upload

---
+++ FirstProgram.s
@@ -0,0 +1,29 @@
+.global _start
+.intel_syntax noprefix
+
+_start:
+	
+	// for writing
+
+	mov rax, 1
+mov rdi, 1
+	lea rsi, [hello_world]
+	mov rdx, 28
+	syscall
+
+
+
+
+
+
+	mov rax, 60
+	mov rdi, 69
+
+
+
+	syscall
+
+
+hello_world:
+	.asciz "Jay Shree Krishn\n"
+	.asciz "Jay Ganesh"
